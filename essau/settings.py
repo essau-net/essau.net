f2dbb7b5fd8d01d5573206267a42832929b3cd9c
Hector Esau <hecttoressau@gmail.com>
2021-08-09 22:39:10 -0500

    Fixing somethin that i broke up before

--- essau/settings.py
+++ essau/settings.py
@@ -144,13 +144,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATIC_ROOT = BASE_DIR / 'static'
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    BASE_DIR/'static',
-    BASE_DIR/'posts_files',
-)
+
 
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
